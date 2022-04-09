183ffe44071b82a1f9739de0abc6b33e09056ef0
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-04-08 21:50:56 -0600

    Clase Nodo

---
+++ Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo{
+    
+    int key;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    public Nodo(int item){
+        key = item;
+        izquierdo = null;
+        derecho = null;
+    }
+
+}
