ba51cb5e3cd669bc350f83f5630d9f8b75fd563c
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2022-04-08 22:35:54 -0600

    Actualizacion Clase Archivo

--- Archivo.java
+++ Archivo.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.util.Scanner;
 
 /**
-* Clase Archivo que lee los archivos de texto y diccionario.
+* Clase Archivo que lee y edita los archivos de texto y diccionario.
 * @author Linda Ines Jimenez Vides
 * @version 8 de abril de 2022
 */
@@ -48,6 +49,45 @@ public class Archivo{
         }
     }
 
+    public void editarDiccionario(){
+        try {
+            //Objeto FileWriter para escribir y guradar las publicaciones en el archivo
+            FileWriter vocab = new FileWriter("diccionario.txt", true);
+            Scanner sn = new Scanner(System.in);
+
+            //.write para guardar en el archivo la infromacion de la publicacion
+            System.out.println("Ingrese el vocabulario de la siguiente manera: ingles,espanol,frances:  ");
+            String vocabulario = sn.nextLine();
+            vocab.write(vocabulario);
+            vocab.write("\n");
+           
+            //Finaliza la escritura en el archivo
+            vocab.close();
+
+        } catch(Exception e) {
+             System.out.println("NO FUE POSIBLE AGREGAR EL VOCABULARIO! INTENTA DE NUEVO");
+        }
+    }
+
+    public void editarTexto(){
+        try {
+            //Objeto FileWriter para escribir y guradar las publicaciones en el archivo
+            FileWriter txt = new FileWriter("texto.txt", true);
+            Scanner sn = new Scanner(System.in);
+
+            //.write para guardar en el archivo la infromacion de la publicacion
+            System.out.println("Ingrese el vocabulario de la siguiente manera: ingles,espanol,frances:  ");
+            String oracion = sn.nextLine();
+            txt.write(oracion);
+            txt.write("\n");
+            //Finaliza la escritura en el archivo
+            txt.close();
+
+        } catch(Exception e) {
+             System.out.println("NO FUE POSIBLE AGREGAR LA ORACION! INTENTA DE NUEVO");
+        }
+    }
+
     public void leerTexto(){
         
         File texto = new File("texto.txt");
@@ -88,4 +128,44 @@ public class Archivo{
             }
         }
     }
+
+        public void imprimirDiccionario(){
+
+            String contenido = " ";
+    
+            try {
+                //Creamos un archivo FileReader que obtiene lo que tenga el archivo
+                FileReader leer = new FileReader("diccionario.txt");
+    
+                //El contenido de lector se guarda en un BufferedReader
+                BufferedReader cont = new BufferedReader(leer);
+    
+                //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+                while((contenido = cont.readLine()) != null) {
+                System.out.println(contenido);
+                }   
+            } catch(Exception e) { 
+                System.out.println("ERROR EN LA CARGA DE VOCABULARIO");
+            }
+        }
+
+        public void imprimirTexto(){
+
+            String contenido = " ";
+    
+            try {
+                //Creamos un archivo FileReader que obtiene lo que tenga el archivo
+                FileReader leer = new FileReader("texto.txt");
+    
+                //El contenido de lector se guarda en un BufferedReader
+                BufferedReader cont = new BufferedReader(leer);
+    
+                //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+                while((contenido = cont.readLine()) != null) {
+                System.out.println(contenido);
+                }   
+            } catch(Exception e) { 
+                System.out.println("ERROR EN LA CARGA DEL TEXTO");
+            }
+        }
 }
